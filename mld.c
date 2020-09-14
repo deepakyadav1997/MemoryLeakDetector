03dd90a8407bb06bd1f2e7de402a3face714fc45
deepak yadav <deepakyadav@localhost.localdomain>
2020-09-14 11:42:26 +0530

    Added structure registration and adding to struct DB

---
+++ mld.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include "mld.h"
+#include "css.h"
+#include<assert.h>
+
+char *DATA_TYPE[] = {"UINT8", "UINT32", "INT32",
+                     "CHAR", "OBJ_PTR", "VOID_PTR", "FLOAT",
+                     "DOUBLE", "OBJ_STRUCT"};
+
+
+int add_structure_to_struct_db(struct_db_t* struct_db,struct_db_rec_t* struct_rec){
+    struct_rec->next = struct_db->head;
+    struct_db->head = struct_rec;
+    struct_db->count++; 
+    return 0;
+}
+//Printing functions
+void print_structure_rec(struct_db_rec_t* struct_rec){
+    if(!struct_rec) return;
+        int j = 0;
+    field_info_t *field = NULL;
+    printf(ANSI_COLOR_BLUE "|------------------------------------------------------|\n" ANSI_COLOR_RESET);
+    printf(ANSI_COLOR_RED "| %-20s | size = %-8d | #flds = %-3d |\n" ANSI_COLOR_RESET, struct_rec->struct_name, struct_rec->ds_size, struct_rec->n_fields);
+    printf(ANSI_COLOR_BLUE "|------------------------------------------------------|------------------------------------------------------------------------------------------|\n" ANSI_COLOR_RESET);
+    for(j = 0; j < struct_rec->n_fields; j++){
+        field = &struct_rec->fields[j];
+        printf("  %-20s |", "");
+        printf("%-3d %-20s | dtype = %-15s | size = %-5d | offset = %-6d|  nstructname = %-20s  |\n",
+                j, field->fname, DATA_TYPE[field->dtype], field->size, field->offset, field->nested_str_name);
+        printf("  %-20s |", "");
+        printf(ANSI_COLOR_BLUE "--------------------------------------------------------------------------------------------------------------------------|\n" ANSI_COLOR_RESET);
+    }
+}
+void print_structure_db(struct_db_t* struct_db){
+    if(!struct_db)
+        return;
+    struct_db_rec_t *current_record = struct_db->head;
+    printf("Total Structures: %d\n",struct_db->count);
+    while(current_record){
+        print_structure_rec(current_record);
+        current_record = current_record->next;
+    }
+}
---
+++ mld.h
@@ -0,0 +1,73 @@
+#ifndef __MLD__
+#define __MLD__
+#include<stdint.h>
+
+#define MAX_STRUCTURE_NAME_SIZE 128
+#define MAX_FIELD_NAME_SIZE 128
+
+typedef enum {
+    UINT8,
+    UINT32,
+    INT32,
+    CHAR,
+    OBJ_PTR,
+    FLOAT,
+    DOUBLE,
+    OBJ_STRUCT
+} data_type_t;
+
+#define OFFSETOF(struct_name,field_name)                            \
+    (intptr_t)&(((struct_name*)0)->field_name)
+
+#define FIELD_SIZE(struct_name,field_name)                          \
+    sizeof(((struct_name*)0)->field_name)
+
+#define FIELD_INFO(struct_name,field_name,dtype,nested_struct_name) \
+    {#field_name,dtype,OFFSETOF(struct_name,field_name),            \
+    FIELD_SIZE(struct_name,field_name),#nested_struct_name}
+
+#define REG_STRUCT(struct_db,str_name,field_array)                  \
+    {                                                               \
+    struct_db_rec_t* rec = calloc(1,sizeof(struct_db_rec_t));       \
+    strncpy(rec->struct_name,#str_name,MAX_STRUCTURE_NAME_SIZE);    \
+    rec->ds_size = sizeof(str_name);                                \
+    rec->n_fields = sizeof(field_array)/sizeof(field_info_t);       \
+    rec->fields = field_array;                                      \
+    if(add_structure_to_struct_db(struct_db,rec) != 0){             \
+        printf("Error!Could not add the structure to the db\n");    \
+        assert(0);                                                  \
+    }}
+
+typedef struct  _struct_db_rec_t_ struct_db_rec_t;
+
+/* Structure to store the info of fields of the structures*/
+typedef  struct field_info{
+    char fname[MAX_FIELD_NAME_SIZE];
+    data_type_t dtype;
+    unsigned int offset;
+    unsigned int size;
+    //below field is only for obj_ptr and obj_struct
+    char nested_str_name[MAX_STRUCTURE_NAME_SIZE];
+}field_info_t;
+
+//Structure to store the structure record
+struct _struct_db_rec_t_{
+    struct_db_rec_t *next; //pointer to the next structure in the linked list
+    char struct_name[MAX_STRUCTURE_NAME_SIZE];//key
+    unsigned int ds_size;   //size of the structure
+    unsigned int n_fields;//no of fields
+    field_info_t* fields; //pointer to the fields
+};
+
+//head of the linkedlist representing the structure db
+typedef struct _struct_db_{
+    struct_db_rec_t* head;
+    unsigned int count;
+}struct_db_t;
+
+//Add structure to the database
+int add_structure_to_struct_db(struct_db_t* struct_db,struct_db_rec_t* struct_rec);
+//Printing functions
+void print_structure_rec(struct_db_rec_t* struct_rec);
+void print_structure_db(struct_db_t* struct_db);
+#endif
